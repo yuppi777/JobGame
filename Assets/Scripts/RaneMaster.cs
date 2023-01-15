2e256d28df8ae70218755bd08c589ccaf2d67ed9
yuppi777 <88840972+yuppi777@users.noreply.github.com>
2023-01-16 00:52:22 +0900

    レーンコミット

---
+++ Assets/Scripts/RaneMaster.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaneMaster : MonoBehaviour
+{
+    [SerializeField] 
+    private List<GameObject> _rane;
+
+    GameObject[] step = new GameObject[10];
+
+    [SerializeField] 
+    [Header("レーンの動くスピード")]
+    private float speed;
+
+
+    
+    private float disappear = -10;
+
+    
+    private float respawn = 30;
+         
+    void Start()
+    {
+        for (int i = 0; i < step.Length; i++)
+        {
+            step[i] = Instantiate(_rane[i], new Vector3(4 * i, 0, 0), Quaternion.identity);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < step.Length; i++)
+        {
+            step[i].gameObject.transform.position -= new Vector3(speed * Time.deltaTime, 0, 0);
+            if (step[i].gameObject.transform.position.x < disappear)
+            {
+                step[i].gameObject.transform.position = new Vector3(respawn, 0, 0);
+            }
+        }
+    }
+}
